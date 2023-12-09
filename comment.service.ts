f10e8fdc90a6e3625360af1bf5f59e4cfdd781aa
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 16:41:14 +0200

    Create comment.service.ts

---
+++ comment.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CommentService {
+    private apiUrl = 'https://jsonplaceholder.typicode.com';
+
+    constructor(private http: HttpClient) { }
+
+    getPostComments(postId: string) {
+        return this.http.get<any[]>(`${this.apiUrl}/comments?postId=${postId}`);
+    }
+}
