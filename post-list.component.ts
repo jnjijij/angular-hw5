eab2aa20fa25d4c58c9c82768f527404c1eebdb1
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 21:32:25 +0200

    Update post-list.component.ts

--- post-list.component.ts
+++ post-list.component.ts
@@ -2,21 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from './user.service';
 import { PostService } from './post.service';
+import { Post } from './post.model'; // Імпорт відповідного типу для посту
 
 @Component({
-    selector: 'app-post-list',
-    templateUrl: './post-list.component.html',
-    styleUrls: ['./post-list.component.css']
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-    posts: any[];
+  posts: Post[];
 
-    constructor(private route: ActivatedRoute, private userService: UserService, private postService: PostService) { }
+  constructor(private route: ActivatedRoute, private userService: UserService, private postService: PostService) { }
 
-    ngOnInit() {
-        const userId = this.route.snapshot.paramMap.get('id');
-        this.userService.getUserPosts(userId).subscribe(posts => {
-            this.posts = posts;
-        });
-    }
+  ngOnInit() {
+    const userId = this.route.snapshot.paramMap.get('id');
+    this.userService.getUserPosts(userId).subscribe(posts => {
+      this.posts = posts;
+    });
+  }
 }
