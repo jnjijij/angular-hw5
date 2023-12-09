48e3e2ce24c43dc06f21c1a13b88feef3bda1cb0
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 16:43:16 +0200

    Create post.service.ts

---
+++ post.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+    private apiUrl = 'https://jsonplaceholder.typicode.com';
+
+    constructor(private http: HttpClient) { }
+
+    getPost(postId: string) {
+        return this.http.get<any>(`${this.apiUrl}/posts/${postId}`);
+    }
+}
