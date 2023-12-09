ced9a558101bd130c0dfe5852e79f984d44723cf
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 16:38:01 +0200

    Create 2 user-details.component.ts

---
+++ 2 user-details.component.ts
@@ -0,0 +1,40 @@
+// user-details.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from './user.service';
+
+@Component({
+    selector: 'app-user-details',
+    template: `
+    <div *ngIf="user">
+      <p>User Details:</p>
+      <p>{{ user.id }} - {{ user.name }}</p>
+      <!-- Додайте інші поля користувача -->
+      <button (click)="loadPosts()">Posts of Current User</button>
+      <div *ngFor="let post of posts">
+        <p>{{ post.title }}</p>
+        <button (click)="navigateToPostDetails(post.id)">Post Details</button>
+      </div>
+    </div>
+  `,
+})
+export class UserDetailsComponent implements OnInit {
+    user: any;
+    posts: any[];
+
+    constructor(private route: ActivatedRoute, private userService: UserService) {}
+
+    ngOnInit() {
+        const userId = this.route.snapshot.params['id'];
+        this.userService.getUserById(userId).subscribe(user => this.user = user);
+    }
+
+    loadPosts(): void {
+        const userId = this.route.snapshot.params['id'];
+        this.userService.getPostsByUser(userId).subscribe(posts => this.posts = posts);
+    }
+
+    navigateToPostDetails(postId: number): void {
+        this.router.navigate(['/post-details', postId]);
+    }
+}
