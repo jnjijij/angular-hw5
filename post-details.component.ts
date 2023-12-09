7c3ef6dcb3d97c3760d5968c7209a6eba461562d
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 21:10:00 +0200

    Update post-details.component.ts

--- post-details.component.ts
+++ post-details.component.ts
@@ -4,23 +4,23 @@ import { PostService } from './post.service';
 import { CommentService } from './comment.service';
 
 @Component({
-    selector: 'app-post-details',
-    templateUrl: './post-details.component.html',
-    styleUrls: ['./post-details.component.css']
+  selector: 'app-post-details',
+  templateUrl: './post-details.component.html',
+  styleUrls: ['./post-details.component.css']
 })
 export class PostDetailsComponent implements OnInit {
-    post: any;
-    comments: any[];
+  post: Post;
+  comments: Comment[];
 
-    constructor(private route: ActivatedRoute, private postService: PostService, private commentService: CommentService) { }
+  constructor(private route: ActivatedRoute, private postService: PostService, private commentService: CommentService) { }
 
-    ngOnInit() {
-        const postId = this.route.snapshot.paramMap.get('id');
-        this.postService.getPost(postId).subscribe(post => {
-            this.post = post;
-        });
-        this.commentService.getPostComments(postId).subscribe(comments => {
-            this.comments = comments;
-        });
-    }
+  ngOnInit() {
+    const postId = this.route.snapshot.paramMap.get('id');
+    this.postService.getPost(postId).subscribe(post => {
+      this.post = post;
+    });
+    this.commentService.getPostComments(postId).subscribe(comments => {
+      this.comments = comments;
+    });
+  }
 }
