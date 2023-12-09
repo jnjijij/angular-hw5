33c5e2d1a7a20b8ffc66bfc874ab42462bdde09b
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 21:14:13 +0200

    Update user-list.component.ts

--- user-list.component.ts
+++ user-list.component.ts
@@ -4,24 +4,24 @@ import { UserService } from './user.service';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-user-list',
-    template: `
-    <div *ngFor="let user of users">
-      <p>{{ user.id }} - {{ user.name }}</p>
-      <button (click)="navigateToUserDetails(user.id)">User Details</button>
-    </div>
-  `,
+  selector: 'app-user-list',
+  template: `
+  <div *ngFor="let user of users">
+   <p>{{ user.id }} - {{ user.name }}</p>
+   <button (click)="navigateToUserDetails(user.id)">User Details</button>
+  </div>
+ `,
 })
 export class UserListComponent implements OnInit {
-    users: any[];
+  users: User[];
 
-    constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService, private router: Router) {}
 
-    ngOnInit() {
-        this.userService.getUsers().subscribe(users => this.users = users);
-    }
+  ngOnInit() {
+    this.userService.getUsers().subscribe(users => this.users = users);
+  }
 
-    navigateToUserDetails(userId: number): void {
-        this.router.navigate(['/user-details', userId]);
-    }
+  navigateToUserDetails(userId: number): void {
+    this.router.navigate(['/user-details', userId]);
+  }
 }
