df075e2e3487e96434ebb42e11e289f323304915
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 21:38:24 +0200

    Update 1user.service.ts

--- 1user.service.ts
+++ 1user.service.ts
@@ -1,33 +1,23 @@
 // user.service.ts
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from './user.service';
+import { User } from './user.model'; // Імпорт відповідного типу для користувача
 
-@Injectable({
-    providedIn: 'root'
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
 })
-export class UserService {
-    private apiUrl = 'https://jsonplaceholder.typicode.com';
+export class UserDetailsComponent implements OnInit {
+  user: User;
 
-    constructor(private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
-    getUsers(): Observable<any[]> {
-        return this.http.get<any[]>(`${this.apiUrl}/users`);
-    }
-
-    getUserById(userId: number): Observable<any> {
-        return this.http.get<any>(`${this.apiUrl}/users/${userId}`);
-    }
-
-    getPostsByUser(userId: number): Observable<any[]> {
-        return this.http.get<any[]>(`${this.apiUrl}/users/${userId}/posts`);
-    }
-
-    getPostById(postId: number): Observable<any> {
-        return this.http.get<any>(`${this.apiUrl}/posts/${postId}`);
-    }
-
-    getCommentsByPost(postId: number): Observable<any[]> {
-        return this.http.get<any[]>(`${this.apiUrl}/posts/${postId}/comments`);
-    }
+  ngOnInit() {
+    const userId = this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(userId).subscribe(user => {
+      this.user = user;
+    });
+  }
 }
