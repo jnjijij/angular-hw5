be00315a4c6a2e1327808c2c23529e10ba4fa492
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-09 21:23:10 +0200

    Update post.service.ts

--- post.service.ts
+++ post.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PostService {
-    private apiUrl = 'https://jsonplaceholder.typicode.com';
+  private apiUrl = 'https://jsonplaceholder.typicode.com';
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    getPost(postId: string) {
-        return this.http.get<any>(`${this.apiUrl}/posts/${postId}`);
-    }
+  getPost(postId: string) {
+    return this.http.get<Post>(`${this.apiUrl}/posts/${postId}`);
+  }
 }
